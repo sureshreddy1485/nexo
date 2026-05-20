937b8b6c2c3dda3d9cea5e1251187aec03624ac1
SureshReddy1485 <sureshreddymallidi45@gmail.com>
2026-05-20 14:02:56 +0530

    fix: use PATCH for edit route to avoid PUT routing conflict

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -13,7 +13,7 @@ router.post('/', protect, upload.single('media'), sendMessage);
 router.get('/:chatId', protect, getMessages);
 router.put('/:chatId/read', protect, markAsRead);
 router.delete('/:id', protect, deleteMessage);
-router.put('/:id/edit', protect, editMessage);
+router.patch('/:id/edit', protect, editMessage);
 router.post('/:id/react', protect, reactToMessage);
 router.post('/:id/forward', protect, forwardMessage);
 router.post('/:id/save', protect, saveMessage);
